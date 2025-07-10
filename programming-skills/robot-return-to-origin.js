65b12cc5c080def71d7144c9a1b32f87546b101b
LaudeDignus <ahmedwade001@gmail.com>
2025-07-10 21:04:58 +0000

    Refactor judgeCircle function by removing commented-out code for clarity

--- programming-skills/robot-return-to-origin.js
+++ programming-skills/robot-return-to-origin.js
@@ -1,20 +1,3 @@
-// const direction = {
-//         "U": [0, 1],
-//         "D": [0, -1],
-//         "R": [1, 0],
-//         "L": [-1, 0]
-//     }
-
-//     let x = 0;
-//     let y = 0;
-
-//     for (let move of moves) {
-//         const [dx, dy] = direction[move];
-//         x += dx;
-//         y += dy;
-//     }
-//     return x === 0 && y === 0
-
 const judgeCircle = (moves) => {
   let x = 0;
   let y = 0;
